735c5c967e296d219c9d80749a04b311f67fba9e
Casey O'Rourke <caseface31@gmail.com>
2017-08-22 22:23:32 -0400

    init commit

---
+++ SpaceInvaders/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager Instance;
+
+	public AudioClip AlienBuzz1;
+	public AudioClip AlienBuzz2;
+	public AudioClip AlienDies;
+	public AudioClip bulletFire;
+	public AudioClip shipExplosion;
+
+	private AudioSource soundEffectAudio;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance == null) {
+			Instance = this;		
+		} else if (Instance != this) {
+			Destroy (gameObject);
+		}
+
+		AudioSource theSource = GetComponent<AudioSource> ();
+		soundEffectAudio = theSource;
+	}
+	
+	public void PlayOneShot(AudioClip clip){
+		soundEffectAudio.PlayOneShot (clip);
+	}
+}
---
+++ SpaceInvaders/Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour {
+
+	public float speed = 30;
+
+	// public GameObject theBullet;
+
+	void FixedUpdate(){
+		float horzMove = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (horzMove, 0) * speed;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			// Fire bullet
+			// Fire bullet sound
+		}
+	}
+}
